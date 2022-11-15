e7e59ecd0b04fcdbb0a185f812220d588bb62b3b
Parbesh Tiwari <88922178+prabeshtiwari1414@users.noreply.github.com>
2022-11-15 21:22:52 +0545

    Add files via upload

---
+++ forloop/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Name
+{
+    class Program
+    {
+        public static void Main(string [] args ){
+            for (int i=0; i<5; i++){
+                for (int j=0; j<=i; j++){
+                    Console.Write("*");
+                }
+                Console.WriteLine(" ");
+            }
+        } 
+    }
+}
