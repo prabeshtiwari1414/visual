6af12b95d11dbc6f97fbc4073aef977517153248
Prabesh Tiwari <prabeshtiwari1414@gmail.com>
2022-11-16 18:17:51 +0545

    adding file
    Signed-off-by: Prabesh Tiwari <prabeshtiwari1414@gmail.com>

---
+++ flag/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace Name
+{
+    class Program
+    {
+        public static void Main(String [] args){
+            for(int i=0; i<=1; i++){
+                for(int j=0; j<=5; j++){
+                    for(int k=0; k<=j; k++){
+                        Console.Write("*");
+                    }
+                    Console.WriteLine("");
+                }
+            }
+        }
+    }
+}
