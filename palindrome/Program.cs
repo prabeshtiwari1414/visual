1e59635a6e8b2fef033397ec2ff0a513aaa50938
Prabesh Tiwari <prabeshtiwari1414@gmail.com>
2022-11-16 19:02:12 +0545

    Adding file
    Signed-off-by: Prabesh Tiwari <prabeshtiwari1414@gmail.com>

---
+++ palindrome/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace Name
+{
+    class Program
+    {
+        public static void Main(String [] args){
+            int n=3443, rev=0, rem, num=n;
+            while (n!=0){
+                rem=n%10;
+                rev=rev*10+rem;
+                n=n/10;
+            }
+            if(num==rev){
+                Console.WriteLine("Palindrome");
+            }
+            else{
+                Console.WriteLine("Not Palindrome");
+            }
+        }
+    }
+}
