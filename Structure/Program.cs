c844489f6d0872c3ac45286720a6f05781584070
Prabesh Tiwari <prabeshtiwari1414@gmail.com>
2022-12-07 19:59:57 +0545

    Structure
    Signed-off-by: Prabesh Tiwari <prabeshtiwari1414@gmail.com>

---
+++ Structure/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace Name
+{
+    public struct Person
+    {
+        public string name;
+        public int age;
+        public int weight;
+
+    }
+    class Student{
+        static void Main(String [] args){
+            Person p1;
+            p1.name="Prabesh Tiwari";
+            p1.age=18;
+            p1.weight=55;
+            Person p2;
+            p2=p1;
+            Console.WriteLine("Values stored in p1");
+            Console.WriteLine("Name:"+p1.name);
+            Console.WriteLine("Age:"+p1.age);
+            Console.WriteLine("Weight:"+p1.weight);
+            Console.WriteLine();
+            Console.WriteLine("Value stored in p2");
+            Console.WriteLine("Name:"+p2.name);
+            Console.WriteLine("Age:"+p2.age);
+            Console.WriteLine("Weight:"+p2.weight);            
+        }
+    }
+}
