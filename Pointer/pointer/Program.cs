90ec8d42dddbbdd600a61eb2450e4543d72e8ec3
Parbesh Tiwari <prabeshtiwari1414@gmail.com>
2023-02-03 22:04:20 +0545

    pointer

---
+++ Pointer/pointer/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Name
+{
+    class Program
+    {
+      unsafe void Print(int* p){
+        Console.WriteLine((int)p);
+      }
+      unsafe static void Main(String [] args){
+        Program p1 = new Program();
+        int i=42;
+        int* p=&i;
+        p1.Print(p);
+      }
+    }
+}
---
+++ Pointer/swapmemory/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace Name
+{
+    class Program
+    {
+        public unsafe void Swap(int*p,int*q){
+            int temp=*q;
+            *p=*q;
+            *q=temp;
+        }
+        public unsafe static void Main(String [] args){
+            Program p = new Program();
+            int var1=10;
+            int var2=20;
+            int* x=&var1;
+            int* y=&var2;
+            Console.WriteLine("Before Swap:var1={0},var2={1}",var1,var2);
+            p.Swap(x,y);
+            Console.WriteLine("After Swap: var1:{0},var2:{1)",var1,var2);
+           
+        }
+    }
+}
