2a516f92cb6d7ae7210d997fff548d790aed0db7
Parbesh Tiwari <prabeshtiwari1414@gmail.com>
2023-02-04 12:49:11 +0545

    ..

---
+++ armstrong/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter your number to find armstrong");
+        int num= Convert.ToInt32(Console.ReadLine());
+        int result=0;
+        int cube;
+        int rem;
+        int temp=num;
+        
+        while(num!=0){
+            rem=num%10;
+            cube=rem*rem*rem;
+            result=result+cube;
+            num=num/10;
+        }
+        if(temp==result){
+            Console.WriteLine("Armstrong number");
+        }
+        else{
+            Console.WriteLine("Not a armstrong number");
+        }
+    }
+}
