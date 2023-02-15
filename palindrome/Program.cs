e7531ac0b9076a45dfaed295cd3f5a9feb2989e7
prabesh <prabeshtiwari1414@gmail.com>
2023-02-15 21:37:32 +0545

    changes

--- palindrome/Program.cs
+++ palindrome/Program.cs
@@ -4,7 +4,9 @@ namespace Name
     class Program
     {
         public static void Main(String [] args){
-            int n=3443, rev=0, rem, num=n;
+            Console.WriteLine("Enter Any number to check palindrome");
+            int n= Convert.ToInt32(Console.ReadLine());
+            int rev=0, rem, num=n;
             while (n!=0){
                 rem=n%10;
                 rev=rev*10+rem;
---
+++ reversedarray/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Name
+{
+    class Program
+    {
+        public static void Main(String []args){
+            int[] arr= new int[]{2,4,6,8,16,32};
+            Array.Reverse(arr);
+            foreach(int newarr in arr){
+                Console.Write(""+newarr+" ");
+            }
+        }
+    }
+}
