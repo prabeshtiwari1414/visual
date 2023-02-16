8577a6386973403a991d6a48708e24ab84b1b8db
prabesh <prabeshtiwari1414@gmail.com>
2023-02-16 07:55:13 +0545

    array

---
+++ singledimesional_array/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Declare and initialize an integer array of size 5
+        int[] nums = { 10, 20, 30, 40, 50 };
+
+        // Access and print each element in the array
+        for (int i = 0; i < nums.Length; i++)
+        {
+            Console.WriteLine("Element at index {0} is {1}", i, nums[i]);
+        }
+
+        // Modify an element in the array
+        nums[2] = 35;
+
+        // Access and print each element in the modified array
+        for (int i = 0; i < nums.Length; i++)
+        {
+            Console.WriteLine("Element at index {0} is {1}", i, nums[i]);
+        }
+    }
+}
