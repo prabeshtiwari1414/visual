0bc998292c3dbc20749c7de79913c1e5e7877907
prabesh <prabeshtiwari1414@gmail.com>
2023-03-04 09:44:11 +0545

    DataBase Connection

---
+++ DBconnection/Program.cs
@@ -0,0 +1,12 @@
+﻿using MySql.Data.MySqlClient;
+
+string cs = @"server=localhost;userid=dbuser;password=s$cret;database=mydb";
+
+using var con = new MySqlConnection(cs);
+con.Open();
+
+var stm = "SELECT VERSION()";
+var cmd = new MySqlCommand(stm, con);
+
+var version = cmd.ExecuteScalar().ToString();
+Console.WriteLine($"MySQL version: {version}");
